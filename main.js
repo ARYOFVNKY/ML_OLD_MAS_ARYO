d58e11f6d68c1aca87c47ecff98fbd9230a09b97
ARYOFVNKY <vrexxzy@gmail.com>
2025-07-07 03:41:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 function mulaiGame() {
   document.body.innerHTML = `
-    <h2>🎮 GAME DIMULAI 🎮</h2>
-    <p>Fanny Skylark siap push! 🔥</p>
+    <h2>🔥 GAME DIMULAI 🔥</h2>
+    <p>Fanny Skylark terbang ke lane! 🛫</p>
     <p>Bot: Jumat Berkah 36 ikut tempur 💥</p>
-    <audio autoplay>
-      <source src="fanny.mp3" type="audio/mpeg">
-      Audio tidak didukung.
-    </audio>
   `;
 }
